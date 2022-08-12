133d8c5361608ba1b607832bcbbc403d5f4e3940
WuotanStudios <101058770+WuotanStudios@users.noreply.github.com>
2022-08-12 22:32:52 +0200

    left hand ctrl and bow animation done
    left hand ctrl and bow animation done
    i have to bind the ctrl to another bone, so the bone and the animation doesn't affect each other

--- GodoDude Mannequin/Skeleton3D.gd
+++ GodoDude Mannequin/Skeleton3D.gd
@@ -1,59 +1,55 @@
 @tool #activates the tool function
 extends Skeleton3D
 
-
+##---------------------------------------------------------------------------------------------------##
 #Exports the "IK Script Active" Button into the Editor inspector
-@export_enum("IK Tools Active", "IK Tools Inactive") var IK_Script_Active = 1
+@export_enum("IK Tools Active", "IK Tools Inactive") var IK_Script_Active = 1 
+##---------------------------------------------------------------------------------------------------##
 
-@export var bone_name : String
-@export var get_bone_id : int
+##---------------------------------------------------------------------------------------------------##
+@export var bone_name : String #type the bone name in the editor inspector by selecting the Skeleton3D node first
+@export var get_bone_id : int #after typing the name of the bone, the bone ID will stored in this var
+##---------------------------------------------------------------------------------------------------##
 
-@export_node_path(Node3D) var Node3D_Path
+##---------------------------------------------------------------------------------------------------##
+# this are export variables for the function _copy_data_from_BoneID_to_target_Node3D(), see below
+@export_node_path(Node3D) var Node3D_Path  
 @export var Bone_ID : int
 @export_flags("start") var Assign_data_from_target_to_Bone = 0
+##---------------------------------------------------------------------------------------------------##
 
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	#_set_Body_Main_Bone_ALL_loc_rot_scale_data()
-	
-	#_setup_max_bone_axial_rot() #testrun
 	pass
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-# _ready() gets called first and with it the _get_Body_Main_Bone_ALL_loc_rot_scale_data() function
-#after that the Body_Main_Bone_ALL in the 3d editor will change the position to the main_bone "spine"(Bone Nr.0)
+
+
 func _process(delta) -> void:
 	if IK_Script_Active == 1: return
 	
 	if Assign_data_from_target_to_Bone == 1: 
 		#print("Script Aktiv"); 
-		_copy_data_from_BoneID_to_target_Node3D()
-		_use_bone_name_to_find_bone_index_nr()
+		_copy_data_from_BoneID_to_target_Node3D() #a little tool to adjust an object to the local bone pose
+		_use_bone_name_to_find_bone_index_nr() #this is just a little tool to find a bone ID according to its bone name
 		return
 	
-	
-	if IK_Script_Active == 0:
-		_get_Body_Main_Bone_ALL_loc_rot_scale_data()
-		_use_bone_name_to_find_bone_index_nr()
+	if IK_Script_Active == 0: #in this case 0 = active
+		_get_Body_Main_Bone_ALL_loc_rot_scale_data() #this function gets the main bone data (spine bone nr. 0) 
+		_use_bone_name_to_find_bone_index_nr() #this is just a little tool to find a bone ID according to its bone name
+		
+		_get_ctrl_data()
+		
 		return
 		
 	else: pass
 
 
 
-
-func _setup_max_bone_axial_rot(_delta): 
-	pass
-
-
-
 func _get_Body_Main_Bone_ALL_loc_rot_scale_data():
 	var get_tool_for_spine_zero = $IK_Targets/Total_Upper_Body/Body_Main_Bone_ALL.get_global_transform()
 	var make_it_local_for_pose = world_transform_to_global_pose(get_tool_for_spine_zero)
-	#print("Position Main Bone: ", make_it_local_for_pose)
 	set_bone_global_pose_override(0, make_it_local_for_pose, 1, true)
 
 
@@ -65,7 +61,6 @@ func _use_bone_name_to_find_bone_index_nr():
 func _copy_data_from_BoneID_to_target_Node3D():
 	if Assign_data_from_target_to_Bone == 1: 
 		if Bone_ID <= 0 or Node3D_Path == null: print("Bone_ID or/and Node3d_path is missing"); Assign_data_from_target_to_Bone = 0; return
-		
 		else: _get_Bone_data_and_assign_to_target(Bone_ID, Node3D_Path); #Assign_data_from_target_to_Bone = 0
 
 
@@ -73,12 +68,28 @@ func _get_Bone_data_and_assign_to_target(target_bone_ID, Object_to_adjust):
 	var main_bone_data = get_bone_global_pose(target_bone_ID)
 	var bone_global_transform_data = global_pose_to_world_transform(main_bone_data)
 	var object_path_to_string = get_node(Object_to_adjust)
-	
 	object_path_to_string.set_global_transform(bone_global_transform_data)
-	
 	Assign_data_from_target_to_Bone = 0
-	
-	#var make_it_local_for_pose = world_transform_to_global_pose(object_path_to_string.get_global_transform())
-	#set_bone_global_pose_override(target_bone_ID, make_it_local_for_pose, 1, true)
 
+
+
+func _get_ctrl_data():
+	var controller_id
+	var ctrl_position : Vector3
+	
+	
+	var temp_ctrl_position_1 : Vector3 = $Attachm_LeftHand/Left_Arm_Hand_Rot_ctrl.get_rotation()
+	ctrl_position.x = temp_ctrl_position_1.y
+	
+	var temp_ctrl_position_2 : Vector3 = $Attachm_LeftHand/Left_Arm_Hand_Rot_ctrl/Left_Hand_Bow_ctrl.get_position() *10
+	ctrl_position.y = temp_ctrl_position_2.y
+	ctrl_position.z = 0
+	_set_blend2d_by_ctrl_data(ctrl_position)
+
+
+
+
+func _set_blend2d_by_ctrl_data(ctrl_position : Vector3):
+	$L_Hand_ctrl_Anim_Tree.set("parameters/left_h_rot/blend_position", ctrl_position.x)
+	$L_Hand_ctrl_Anim_Tree.set("parameters/left_h_bow/blend_position", ctrl_position.y)
 
